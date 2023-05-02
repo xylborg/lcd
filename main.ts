18470ab189e7af8b8fb5e817800dcaf6555b75d0
xylborg <114168337+xylborg@users.noreply.github.com>
2023-05-02 13:06:58 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+lcd1602.setAddress(
+lcd1602.I2C_ADDR.addr1
+)
+lcd1602.putString(
+"ArcBot",
+3,
+0
+)
+lcd1602.set_LCD_Show(lcd1602.visibled.visible)
+lcd1602.set_backlight(lcd1602.on_off.on)
+let val = 0
+basic.forever(function () {
+    val += 1
+    lcd1602.putNumber(
+    val,
+    7,
+    1
+    )
+    basic.pause(1000)
+})
